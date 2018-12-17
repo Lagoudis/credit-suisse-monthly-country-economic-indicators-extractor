fe3ed409b3c58a8d14ffaa8b958af61eefc17e0e
Lagoudis <33759403+Lagoudis@users.noreply.github.com>
2018-12-17 12:30:50 +0000

    Update ITextPdfService.java

--- credit-suisse-monthly-country-economic-indicators-extractor-common/src/main/java/github/aq/springbootcreditsuissedataextractor/pdf/ITextPdfService.java
+++ credit-suisse-monthly-country-economic-indicators-extractor-common/src/main/java/github/aq/springbootcreditsuissedataextractor/pdf/ITextPdfService.java
@@ -7,6 +7,7 @@ public class ITextPdfService {
 
     public static String extractTextFromPage(String filename, int page) {
         String textReturned = "";
+        while(i<=85){
         try {
             PdfReader reader = new PdfReader(filename);
             // int n = reader.getNumberOfPages();
@@ -15,6 +16,7 @@ public class ITextPdfService {
             reader.close();
         } catch (Exception e) {
             System.out.println(e);
+        }
         }
         return textReturned;
     }
